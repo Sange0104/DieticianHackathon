34008f6a947137fa2d0ea75568f7b511ea6a4ca0
prem5 <prem5@Prem>
2022-06-16 14:37:06 -0400

    first commit

---
+++ src/test/java/stepDefinitions/InvalidRegistrationSteps.java
@@ -0,0 +1,17 @@
+package stepDefinitions;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import PageObjects.RegistrationPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class InvalidRegistrationSteps {
+	
+	
+	
+}
+	
